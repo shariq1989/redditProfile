41da24e9a1eda8f3625634725886b57118cc84dc
Shariq Jamil <shariq1989@gmail.com>
2018-07-08 22:13:23 -0600

    Using newer PRAW API and a JSON file for creds

--- letsProfile.py
+++ letsProfile.py
@@ -11,25 +11,30 @@ import pprint
 import praw
 import operator
 import time
+import json
 
+with open('../config.json') as f:
+    config = json.load(f)
 
-user_agent = ("letsProfile")
-r = praw.Reddit(user_agent=user_agent)
-#user to look up
-user_name = "ENTER_USERNAME"
-#get user profile
-user = r.get_redditor(user_name)
+reddit = praw.Reddit(client_id=config['RedditDevCredentials']['client_id'],
+                     client_secret=config['RedditDevCredentials']['client_secret'],
+                     user_agent=config['RedditDevCredentials']['user_agent'],
+                     )
 
-#GET USER SUBMISSIONS
+# user to look up
+user_name = "shariq1989"  # get user profile
+user = reddit.redditor(user_name)
+
+# GET USER SUBMISSIONS
 start_sub = time.time()
-submissions = user.get_submitted(limit=None)
+submissions = user.submissions.top('all')
 sub_karma_by_subreddit = {}
 for submission in submissions:
     subreddit = submission.subreddit.display_name
     sub_karma_by_subreddit[subreddit] = (sub_karma_by_subreddit.
                                          get(subreddit, 0)
                                          + submission.score)
-sorted_sub = sorted(sub_karma_by_subreddit.items(), key=operator.itemgetter(1),reverse=True)
+sorted_sub = sorted(sub_karma_by_subreddit.items(), key=operator.itemgetter(1), reverse=True)
 print("PRINTING SUMBISSION STATS")
 pprint.pprint(sorted_sub)
 end_sub = time.time()
@@ -38,16 +43,17 @@ print("------")
 print("PRINTING COMMENT STATS")
 print("------")
 
-#GET USER COMMENTS
+# GET USER COMMENTS
 start_comm = time.time()
-comments = user.get_comments(limit=None)
+comments = user.comments.new(limit=500)
 comm_karma_by_subreddit = {}
 for comment in comments:
-    comm_karma_by_subreddit[comment.subreddit.display_name] = (comm_karma_by_subreddit.get(comment.subreddit.display_name, 0)
-                                     + comment.score)
-sorted_comm = sorted(comm_karma_by_subreddit.items(), key=operator.itemgetter(1),reverse=True)
+    comm_karma_by_subreddit[comment.subreddit.display_name] = (
+        comm_karma_by_subreddit.get(comment.subreddit.display_name, 0)
+        + comment.score)
+sorted_comm = sorted(comm_karma_by_subreddit.items(), key=operator.itemgetter(1), reverse=True)
 pprint.pprint(sorted_comm)
 end_comm = time.time()
 
-print("Collecting post info took " + str("%.2f" %(end_sub - start_sub)) + " seconds.")
-print("Collecting comment info took " + str("%.2f" %(end_comm - start_comm)) + " seconds.")
+print("Submission collection:" + str("%.2f" % (end_sub - start_sub)) + " seconds.")
+print("Comment collection:" + str("%.2f" % (end_comm - start_comm)) + " seconds.")
