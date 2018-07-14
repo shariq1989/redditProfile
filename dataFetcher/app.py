758d79090e7cf6f6e4718f108ea57f329b60ecde
Rick Jamil <shariq1989@gmail.com>
2018-07-13 23:46:44 -0600

    added Flask and REST endpoint

---
+++ dataFetcher/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_cors import CORS
+
+# configuration
+from letsProfile import getProfile;
+
+DEBUG = True
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app)
+
+
+# sanity check route
+@app.route('/profile/<user_id>', methods=['GET'])
+def get_profile(user_id):
+    return getProfile(user_id);
+
+
+if __name__ == '__main__':
+    app.run()
--- dataFetcher/letsProfile.py
+++ dataFetcher/letsProfile.py
@@ -14,47 +14,49 @@ import time
 
 import praw
 
-with open('config.json') as f:
-    config = json.load(f)
 
-reddit = praw.Reddit(client_id=config['RedditDevCredentials']['client_id'],
-                     client_secret=config['RedditDevCredentials']['client_secret'],
-                     user_agent=config['RedditDevCredentials']['user_agent'],
-                     )
+def getProfile(user_id):
+    with open('config.json') as f:
+        config = json.load(f)
 
-# user to look up
-user_name = "shariq1989"  # get user profile
-user = reddit.redditor(user_name)
+    reddit = praw.Reddit(client_id=config['RedditDevCredentials']['client_id'],
+                         client_secret=config['RedditDevCredentials']['client_secret'],
+                         user_agent=config['RedditDevCredentials']['user_agent'],
+                         )
 
-# GET USER SUBMISSIONS
-start_sub = time.time()
-submissions = user.submissions.top('all')
-sub_karma_by_subreddit = {}
-for submission in submissions:
-    subreddit = submission.subreddit.display_name
-    sub_karma_by_subreddit[subreddit] = (sub_karma_by_subreddit.
-                                         get(subreddit, 0)
-                                         + submission.score)
-sorted_sub = sorted(sub_karma_by_subreddit.items(), key=operator.itemgetter(1), reverse=True)
-print("PRINTING SUMBISSION STATS")
-pprint.pprint(sorted_sub)
-end_sub = time.time()
+    # user to look up
+    user_name = user_id  # get user profile
+    user = reddit.redditor(user_name)
 
-print("------")
-print("PRINTING COMMENT STATS")
-print("------")
+    # GET USER SUBMISSIONS
+    start_sub = time.time()
+    submissions = user.submissions.top('all')
+    sub_karma_by_subreddit = {}
+    for submission in submissions:
+        subreddit = submission.subreddit.display_name
+        sub_karma_by_subreddit[subreddit] = (sub_karma_by_subreddit.
+                                             get(subreddit, 0)
+                                             + submission.score)
+    sorted_sub = sorted(sub_karma_by_subreddit.items(), key=operator.itemgetter(1), reverse=True)
+    print("PRINTING SUMBISSION STATS")
+    pprint.pprint(sorted_sub)
+    end_sub = time.time()
 
-# GET USER COMMENTS
-start_comm = time.time()
-comments = user.comments.new(limit=500)
-comm_karma_by_subreddit = {}
-for comment in comments:
-    comm_karma_by_subreddit[comment.subreddit.display_name] = (
-        comm_karma_by_subreddit.get(comment.subreddit.display_name, 0)
-        + comment.score)
-sorted_comm = sorted(comm_karma_by_subreddit.items(), key=operator.itemgetter(1), reverse=True)
-pprint.pprint(sorted_comm)
-end_comm = time.time()
+    print("------")
+    print("PRINTING COMMENT STATS")
+    print("------")
 
-print("Submission collection:" + str("%.2f" % (end_sub - start_sub)) + " seconds.")
-print("Comment collection:" + str("%.2f" % (end_comm - start_comm)) + " seconds.")
+    # GET USER COMMENTS
+    start_comm = time.time()
+    comments = user.comments.new(limit=500)
+    comm_karma_by_subreddit = {}
+    for comment in comments:
+        comm_karma_by_subreddit[comment.subreddit.display_name] = (
+            comm_karma_by_subreddit.get(comment.subreddit.display_name, 0)
+            + comment.score)
+    sorted_comm = sorted(comm_karma_by_subreddit.items(), key=operator.itemgetter(1), reverse=True)
+    pprint.pprint(sorted_comm)
+    end_comm = time.time()
+    print("Submission collection:" + str("%.2f" % (end_sub - start_sub)) + " seconds.")
+    print("Comment collection:" + str("%.2f" % (end_comm - start_comm)) + " seconds.")
+    return json.dumps(sorted_sub);
