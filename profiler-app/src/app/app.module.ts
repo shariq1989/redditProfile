9e767348e17f802472f033bbd15636da2c07393d
Rick <shariq1989@gmail.com>
2018-07-19 21:50:01 -0600

    Two way binding

--- profiler-app/src/app/app.module.ts
+++ profiler-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import {
   MatButtonModule,
   MatMenuModule,
@@ -19,6 +20,7 @@ import { SearchProfileComponent } from './search-profile/search-profile.componen
     SearchProfileComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
