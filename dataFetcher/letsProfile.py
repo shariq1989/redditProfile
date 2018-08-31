d88f146c8d4261525fb5d0f04cdbb6667f98c53f
Rick Jamil <shariq1989@gmail.com>
2018-08-30 21:27:25 -0600

    Backend Error Handling

--- dataFetcher/letsProfile.py
+++ dataFetcher/letsProfile.py
@@ -9,6 +9,7 @@
 
 import json
 import time
+
 import praw
 
 
@@ -30,22 +31,29 @@ def getProfile(user_id):
     comments = user.comments.new(limit=500)
 
     subreddit_list = {}
+    try:
+        for comment in comments:
+            if comment.subreddit.display_name not in subreddit_list:
+                subreddit_list[comment.subreddit.display_name] = 1
+            else:
+                score = subreddit_list[comment.subreddit.display_name]
+                subreddit_list[comment.subreddit.display_name] = score + 1
+    except Exception as e:
+        print('EXCEPTION IN COMMENT COLLECTION')
+        print(e)
 
-    for comment in comments:
-        if comment.subreddit.display_name not in subreddit_list:
-            subreddit_list[comment.subreddit.display_name] = 1
-        else:
-            score = subreddit_list[comment.subreddit.display_name]
-            subreddit_list[comment.subreddit.display_name] = score + 1
-
-    for submission in submissions:
-        if submission.subreddit.display_name not in subreddit_list:
-            subreddit_list[submission.subreddit.display_name] = 1
-        else:
-            score = subreddit_list[submission.subreddit.display_name]
-            subreddit_list[submission.subreddit.display_name] = score + 1
+    try:
+        for submission in submissions:
+            if submission.subreddit.display_name not in subreddit_list:
+                subreddit_list[submission.subreddit.display_name] = 1
+            else:
+                score = subreddit_list[submission.subreddit.display_name]
+                subreddit_list[submission.subreddit.display_name] = score + 1
+    except Exception as e:
+        print('EXCEPTION IN SUBMISSION COLLECTION')
+        print(e)
 
     end_fetching_time = time.time()
     print("Submission & Comment collection:" + str("%.2f" % (end_fetching_time - start_fetching_time)) + " seconds.")
     print(subreddit_list)
-    return json.dumps(subreddit_list);
+    return json.dumps(subreddit_list)
